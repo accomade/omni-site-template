a1a1436c273fe89bc4f79779612b0e0facb44c64
Dirk Porsche <dirk@accoma.de>
2025-09-09 22:17:02 +0200

    upgraded deps

--- src/routes/[lang]/[...page]/+page.svelte
+++ src/routes/[lang]/[...page]/+page.svelte
@@ -9,7 +9,7 @@
 
   const pages: Record<string, PageProps> = siteConfig.pages as Record<string, PageProps>;
 
-  let pageParam = $derived(page.params.page);
+  let pageParam = $derived(page.params.page ?? '/');
   let thePage = $derived(pages[pageParam]);
   const ss: SiteState = getContext('SITE_STATE');
 
